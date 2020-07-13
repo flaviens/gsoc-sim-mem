79518ea0f4863cdfa800b482a2c9158137cf0795
Flavien Solt <flsolt@ethz.ch>
2020-07-13 22:31:53 +0200

    Writing the complete, top-level testbench

--- dv/simmem_write_only_nocontent/cpp/simmem_axi_structures.cc
+++ dv/simmem_write_only_nocontent/cpp/simmem_axi_structures.cc
@@ -20,7 +20,7 @@ const uint64_t WriteAddressRequest::protection_type_width = AxProtWidth;
 const uint64_t WriteAddressRequest::qos_width = AxQoSWidth;
 
 // Static constant definition (offsets)
-const uint64_t WriteAddressRequest::id_offset = 0;
+const uint64_t WriteAddressRequest::id_offset = 0UL;
 const uint64_t WriteAddressRequest::addr_offset =
     WriteAddressRequest::id_offset + WriteAddressRequest::id_width;
 const uint64_t WriteAddressRequest::burst_length_offset =
@@ -44,207 +44,124 @@ const uint64_t WriteAddressRequest::qos_offset =
     WriteAddressRequest::protection_type_offset +
     WriteAddressRequest::protection_type_width;
 
-// Method implementations
-WriteAddressRequest::WriteAddressRequest(
-    uint64_t id, uint64_t addr, uint64_t burst_length, uint64_t burst_size,
-    uint64_t burst_type, uint64_t lock_type, uint64_t memory_type,
-    uint64_t protection_type, uint64_t qos) {
-  gen_packed(id, addr, burst_length, burst_size, burst_type, lock_type,
-             memory_type, protection_type, qos);
-}
+void WriteAddressRequest::from_packed(uint64_t packed) {
+  uint64_t low_mask;
 
-WriteAddressRequest::WriteAddressRequest(uint64_t packed) {
-  this->packed = packed;
-}
-
-void WriteAddressRequest::gen_packed(uint64_t id, uint64_t addr,
-                                     uint64_t burst_length, uint64_t burst_size,
-                                     uint64_t burst_type, uint64_t lock_type,
-                                     uint64_t memory_type,
-                                     uint64_t protection_type, uint64_t qos) {
-  packed = 0;
-  set_id(id);
-  set_addr(addr);
-  set_burst_length(burst_length);
-  set_burst_size(burst_size);
-  set_burst_type(burst_type);
-  set_lock_type(lock_type);
-  set_memory_type(memory_type);
-  set_protection_type(protection_type);
-  set_qos(qos);
-}
-
-// Setters
-void WriteAddressRequest::set_id(uint64_t id) {
-  if (!id_width) {
-    return;
+  if (id_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
+    id = low_mask & ((packed & (low_mask << id_offset)) >> id_offset);
   }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
-  packed &= low_mask << id_offset;
-  packed |= (~low_mask & id) << id_offset;
-}
 
-void WriteAddressRequest::set_addr(uint64_t addr) {
-  if (!addr_width) {
-    return;
+  if (addr_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - addr_width);
+    addr = low_mask & ((packed & (low_mask << addr_offset)) >> addr_offset);
   }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - addr_width);
-  packed &= low_mask << addr_offset;
-  packed |= (~low_mask & addr) << addr_offset;
-}
 
-void WriteAddressRequest::set_burst_length(uint64_t burst_length) {
-  if (!burst_length_width) {
-    return;
+  if (burst_length_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_length_width);
+    burst_length = low_mask & ((packed & (low_mask << burst_length_offset)) >>
+                               burst_length_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_length_width);
-  packed &= low_mask << burst_length_offset;
-  packed |= (~low_mask & burst_length) << burst_length_offset;
-}
 
-void WriteAddressRequest::set_burst_size(uint64_t burst_size) {
-  if (!burst_size_width) {
-    return;
+  if (burst_size_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_size_width);
+    burst_size = low_mask & ((packed & (low_mask << burst_size_offset)) >>
+                             burst_size_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_size_width);
-  packed &= low_mask << burst_size_offset;
-  packed |= (~low_mask & burst_size) << burst_size_offset;
-}
 
-void WriteAddressRequest::set_burst_type(uint64_t burst_type) {
-  if (!burst_type_width) {
-    return;
+  if (burst_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_type_width);
+    burst_type = low_mask & ((packed & (low_mask << burst_type_offset)) >>
+                             burst_type_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_type_width);
-  packed &= low_mask << burst_type_offset;
-  packed |= (~low_mask & burst_type) << burst_type_offset;
-}
 
-void WriteAddressRequest::set_lock_type(uint64_t lock_type) {
-  if (!lock_type_width) {
-    return;
+  if (lock_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - lock_type_width);
+    lock_type = low_mask &
+                ((packed & (low_mask << lock_type_offset)) >> lock_type_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - lock_type_width);
-  packed &= low_mask << lock_type_offset;
-  packed |= (~low_mask & lock_type) << lock_type_offset;
-}
 
-void WriteAddressRequest::set_memory_type(uint64_t memory_type) {
-  if (!memory_type_width) {
-    return;
+  if (memory_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - memory_type_width);
+    memory_type = low_mask & ((packed & (low_mask << memory_type_offset)) >>
+                              memory_type_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - memory_type_width);
-  packed &= low_mask << memory_type_offset;
-  packed |= (~low_mask & memory_type) << memory_type_offset;
-}
 
-void WriteAddressRequest::set_protection_type(uint64_t protection_type) {
   if (!protection_type_width) {
-    return;
+    low_mask =
+        (1UL << (PackedWidth - 1)) >> (PackedWidth - protection_type_width);
+    protection_type =
+        low_mask & ((packed & (low_mask << protection_type_offset)) >>
+                    protection_type_offset);
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - protection_type_width);
-  packed &= low_mask << protection_type_offset;
-  packed |= (~low_mask & protection_type) << protection_type_offset;
+
+  if (qos_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - qos_width);
+    qos = low_mask & ((packed & (low_mask << qos_offset)) >> qos_offset);
+  }
 }
 
-void WriteAddressRequest::set_qos(uint64_t qos) {
-  if (!qos_width) {
-    return;
+uint64_t WriteAddressRequest::to_packed() {
+  uint64_t packed = 0UL;
+  uint64_t low_mask;
+
+  if (id_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
+    packed &= low_mask << id_offset;
+    packed |= (~low_mask & id) << id_offset;
   }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - qos_width);
-  packed &= low_mask << qos_offset;
-  packed |= (~low_mask & qos) << qos_offset;
-}
 
-void WriteAddressRequest::set_packed(uint64_t packed) { this->packed = packed; }
-
-// Getters
-uint64_t WriteAddressRequest::get_id(void) {
-  if (!id_width) {
-    return 0;
+  if (addr_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - addr_width);
+    packed &= low_mask << addr_offset;
+    packed |= (~low_mask & addr) << addr_offset;
   }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
-  return (packed & (low_mask << id_offset)) >> id_offset;
-}
 
-uint64_t WriteAddressRequest::get_addr(void) {
-  if (!addr_width) {
-    return 0;
+  if (burst_length_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_length_width);
+    packed &= low_mask << burst_length_offset;
+    packed |= (~low_mask & burst_length) << burst_length_offset;
   }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - addr_width);
-  return (packed & (low_mask << addr_offset)) >> addr_offset;
-}
 
-uint64_t WriteAddressRequest::get_burst_length(void) {
-  if (!burst_length_width) {
-    return 0;
+  if (burst_size_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_size_width);
+    packed &= low_mask << burst_size_offset;
+    packed |= (~low_mask & burst_size) << burst_size_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_length_width);
-  return (packed & (low_mask << burst_length_offset)) >> burst_length_offset;
-}
 
-uint64_t WriteAddressRequest::get_burst_size(void) {
-  if (!burst_size_width) {
-    return 0;
+  if (burst_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_type_width);
+    packed &= low_mask << burst_type_offset;
+    packed |= (~low_mask & burst_type) << burst_type_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_size_width);
-  return (packed & (low_mask << burst_size_offset)) >> burst_size_offset;
-}
 
-uint64_t WriteAddressRequest::get_burst_type(void) {
-  if (!burst_type_width) {
-    return 0;
+  if (lock_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - lock_type_width);
+    packed &= low_mask << lock_type_offset;
+    packed |= (~low_mask & lock_type) << lock_type_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - burst_type_width);
-  return (packed & (low_mask << burst_type_offset)) >> burst_type_offset;
-}
 
-uint64_t WriteAddressRequest::get_lock_type(void) {
-  if (!lock_type_width) {
-    return 0;
+  if (memory_type_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - memory_type_width);
+    packed &= low_mask << memory_type_offset;
+    packed |= (~low_mask & memory_type) << memory_type_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - lock_type_width);
-  return (packed & (low_mask << lock_type_offset)) >> lock_type_offset;
-}
 
-uint64_t WriteAddressRequest::get_memory_type(void) {
-  if (!memory_type_width) {
-    return 0;
+  if (protection_type_width) {
+    low_mask =
+        (1UL << (PackedWidth - 1)) >> (PackedWidth - protection_type_width);
+    packed &= low_mask << protection_type_offset;
+    packed |= (~low_mask & protection_type) << protection_type_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - memory_type_width);
-  return (packed & (low_mask << memory_type_offset)) >> memory_type_offset;
-}
 
-uint64_t WriteAddressRequest::get_protection_type(void) {
-  if (!protection_type_width) {
-    return 0;
+  if (qos_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - qos_width);
+    packed &= low_mask << qos_offset;
+    packed |= (~low_mask & qos) << qos_offset;
   }
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - protection_type_width);
-  return (packed & (low_mask << protection_type_offset)) >>
-         protection_type_offset;
-}
 
-uint64_t WriteAddressRequest::get_qos(void) {
-  if (!qos_width) {
-    return 0;
-  }
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - qos_width);
-  return (packed & (low_mask << qos_offset)) >> qos_offset;
+  return packed;
 }
-
-uint64_t WriteAddressRequest::get_packed(void) { return packed; }
 
 ////////////////////
 // Write response //
@@ -255,49 +172,39 @@ const uint64_t WriteResponse::id_width = IDWidth;
 const uint64_t WriteResponse::content_width = XRespWidth;
 
 // Static constant definition (offsets)
-const uint64_t WriteResponse::id_offset = 0;
+const uint64_t WriteResponse::id_offset = 0UL;
 const uint64_t WriteResponse::content_offset =
     WriteResponse::id_offset + WriteResponse::id_width;
 
-// Method implementations
-WriteResponse::WriteResponse(uint64_t id, uint64_t content) {
-  gen_packed(id, content);
+void WriteResponse::from_packed(uint64_t packed) {
+  uint64_t low_mask;
+
+  if (id_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
+    id = low_mask & ((packed & (low_mask << id_offset)) >> id_offset);
+  }
+
+  if (content_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - content_width);
+    content =
+        low_mask & ((packed & (low_mask << content_offset)) >> content_offset);
+  }
 }
 
-WriteResponse::WriteResponse(uint64_t packed) { this->packed = packed; }
+uint64_t WriteResponse::to_packed() {
+  uint64_t packed = 0UL;
+  uint64_t low_mask;
+  if (!id_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
+    packed &= low_mask << id_offset;
+    packed |= (~low_mask & id) << id_offset;
+  }
 
-void WriteResponse::gen_packed(uint64_t id, uint64_t content) {
-  packed = 0;
-  set_id(id);
-  set_content(content);
+  if (content_width) {
+    low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - content_width);
+    packed &= low_mask << content_offset;
+    packed |= (~low_mask & content) << content_offset;
+  }
+
+  return packed;
 }
-
-// Setters
-void WriteResponse::set_id(uint64_t id) {
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
-  packed &= low_mask << id_offset;
-  packed |= (~low_mask & id) << id_offset;
-}
-
-void WriteResponse::set_content(uint64_t content) {
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - content_width);
-  packed &= low_mask << content_offset;
-  packed |= (~low_mask & content) << content_offset;
-}
-
-void WriteResponse::set_packed(uint64_t content) { this->packed = packed; }
-
-// Getters
-uint64_t WriteResponse::get_id(void) {
-  uint64_t low_mask = (1UL << (PackedWidth - 1)) >> (PackedWidth - id_width);
-  return (packed & (low_mask << id_offset)) >> id_offset;
-}
-
-uint64_t WriteResponse::get_content(void) {
-  uint64_t low_mask =
-      (1UL << (PackedWidth - 1)) >> (PackedWidth - content_width);
-  return (packed & (low_mask << content_offset)) >> content_offset;
-}
-
-uint64_t WriteResponse::get_packed(void) { return packed; }
--- dv/simmem_write_only_nocontent/cpp/simmem_write_only_nocontent_tb.cc
+++ dv/simmem_write_only_nocontent/cpp/simmem_write_only_nocontent_tb.cc
@@ -27,6 +27,8 @@ const size_t kNbLocalIdentifiers = 32;
 const size_t kAdjustmentDelay = 1;  // Cycles to subtract to the actual delay
 
 typedef Vsimmem_write_only_nocontent Module;
+typedef std::map<uint64_t, std::queue<WriteAddressRequest>> waddr_queue_map_t;
+typedef std::map<uint64_t, std::queue<WriteResponse>> wresp_queue_map_t;
 
 // This class implements elementary operations for the testbench
 class SimmemWriteOnlyNoBurstTestbench {
@@ -99,7 +101,7 @@ class SimmemWriteOnlyNoBurstTestbench {
    * @param waddr_req the input address request
    */
   void simmem_requester_waddr_apply(WriteAddressRequest waddr_req) {
-    module_->waddr_data_i = waddr_req.get_packed();
+    module_->waddr_data_i = waddr_req.to_packed();
     module_->waddr_in_valid_i = 1;
   }
 
@@ -125,7 +127,7 @@ class SimmemWriteOnlyNoBurstTestbench {
     module_->eval();
     assert(module_->wresp_out_ready_i);
 
-    out_data.set_packed(module_->wresp_data_o);
+    out_data.from_packed(module_->wresp_data_o);
     return (bool)(module_->wresp_out_valid_o);
   }
 
@@ -141,7 +143,7 @@ class SimmemWriteOnlyNoBurstTestbench {
    * @param wresp the input write response
    */
   void simmem_realmem_wresp_apply(WriteResponse wresp) {
-    module_->wresp_data_i = wresp.get_packed();
+    module_->wresp_data_i = wresp.to_packed();
     module_->wresp_in_valid_i = 1;
   }
 
@@ -167,7 +169,7 @@ class SimmemWriteOnlyNoBurstTestbench {
     module_->eval();
     assert(module_->waddr_out_ready_i);
 
-    out_data.set_packed(module_->waddr_data_o);
+    out_data.from_packed(module_->waddr_data_o);
     return (bool)(module_->waddr_out_valid_o);
   }
 
@@ -200,6 +202,67 @@ class SimmemWriteOnlyNoBurstTestbench {
   VerilatedFstC *trace_;
 };
 
+class RealMemoryController {
+ public:
+  RealMemoryController(std::vector<uint64_t> identifiers) {
+    for (size_t i = 0; i < identifiers.size(); i++) {
+      wresp_queues.insert(std::pair<uint64_t, std::queue<WriteResponse>>(
+          identifiers[i], std::queue<WriteResponse>()));
+    }
+  }
+
+  /**
+   * Simulates immediate operation of the real memory controller.
+   * The messages are arbitrarily issued by lowest AXI identifier first.
+   *
+   * @return 1 iff the real controller holds a valid write response.
+   */
+  bool has_wresp_to_input() {
+    queue_map_t::iterator it;
+    for (it = wresp_queues.begin(); it != wresp_queues.end(); it++) {
+      if (it->second.size()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Gets the next write response. Assumes there is one ready.
+   * This function is not destructive: the write response is not popped.
+   *
+   * @return the write response.
+   */
+  WriteResponse get_next_wresp() {
+    queue_map_t::iterator it;
+    for (it = wresp_queues.begin(); it != wresp_queues.end(); it++) {
+      if (it->second.size()) {
+        return it->second.front();
+      }
+    }
+    assert(true);
+  }
+
+  /**
+   * Gets the next write response. Assumes there is one ready.
+   * This function is destructive: the write response is popped.
+   *
+   * @return the write response.
+   */
+  uint64_t pop_next_wresp() {
+    queue_map_t::iterator it;
+    for (it = wresp_queues.begin(); it != wresp_queues.end(); it++) {
+      if (it->second.size()) {
+        return it->second.pop();
+      }
+    }
+    assert(true);
+  }
+
+ private:
+  wresp_queue_map_t wresp_queues;
+}
+
 void simple_testbench(SimmemWriteOnlyNoBurstTestbench *tb) {
   tb->simmem_reset();
 
@@ -208,6 +271,181 @@ void simple_testbench(SimmemWriteOnlyNoBurstTestbench *tb) {
   while (!tb->simmem_is_done()) {
     tb->simmem_tick();
   }
+}
+
+void randomized_testbench(SimmemWriteOnlyNoBurstTestbench *tb,
+                          size_t num_identifiers, unsigned int seed) {
+  srand(seed);
+
+  int nb_iterations = 1000;
+
+  std::vector<uint64_t> identifiers;
+
+  for (size_t i = 0; i < num_identifiers; i++) {
+    identifiers.push_back(i);
+  }
+
+  RealMemoryController realmem(identifiers);
+
+  waddr_queue_t waddr_queues;
+  wresp_queue_t wresp_queues;
+
+  for (size_t i = 0; i < num_identifiers; i++) {
+    waddr_queues.insert(std::pair<uint64_t, std::queue<WriteAddressRequest>>(
+        identifiers[i], std::queue<WriteAddressRequest>()));
+    wresp_queues.insert(std::pair<uint64_t, std::queue<WriteResponse>>(
+        identifiers[i], std::queue<WriteResponse>()));
+  }
+
+  bool requester_apply_input_data;
+  bool realmem_apply_input_data;
+  bool requester_req_output_data;
+  bool realmem_req_output_data;
+
+  bool iteration_announced;  // Variable only used for display
+
+  WriteAddressRequest requester_current_input;  // Input from the requester
+  requester_current_input
+      .from_packed(rand() % PackedWidth)
+          requester_current_input.id = identifiers[rand() % num_identifiers];
+
+  WriteResponse requester_current_output;  // Output to the requester
+
+  uint64_t realmem_current_input;   // Input from the real memory controller
+  uint64_t realmem_current_output;  // Output to the real memory controller
+
+  tb->simmem_reset();
+
+  for (size_t i = 0; i < nb_iterations; i++) {
+    iteration_announced = false;
+
+    // Randomize the boolean signals deciding which interactions will take place
+    // in this cycle
+    requester_apply_input_data = (bool)(rand() & 1);
+    requester_req_output_data = (bool)(rand() & 1);
+    realmem_apply_input_data =
+        realmem.has_write_data_to_input(realmem_current_output);
+    // The real memory controller is supposedly always ready to get data
+    realmem_req_output_data = 1;
+
+    if (requester_apply_input_data) {
+      // Apply a given input
+      tb->simmem_requester_waddr_apply(current_input_id, current_content);
+    }
+    if (requester_req_output_data) {
+      // Try to fetch an output if the handshake is successful
+      tb->simmem_requester_wresp_request();
+    }
+    // if (simmem_apply_input_data) {
+    //   // Apply a given input
+    //   tb->simmem_requester_waddr_apply(current_input_id, current_content);
+    // }
+    // if (requester_req_output_data) {
+    //   // Try to fetch an output if the handshake is successful
+    //   tb->simmem_requester_wresp_request();
+    // }
+
+    // TODO: Continue here by adding the interaction with the real memory
+    // controller
+
+    // Only perform the evaluation once all the inputs have been applied
+    if (reserve && tb->simmem_reservation_check()) {
+      if (kTransactionsVerbose) {
+        if (!iteration_announced) {
+          iteration_announced = true;
+          std::cout << std::endl << "Step " << std::dec << i << std::endl;
+        }
+        std::cout << current_reservation_id << " reserves "
+                  << tb->simmem_reservation_get_address() << std::endl;
+      }
+
+      // Renew the reservation identifier if the reservation has been successful
+      current_reservation_id = identifiers[rand() % num_identifiers];
+    }
+    if (tb->simmem_input_data_check()) {
+      // If the input handshake has been successful, then add the input into the
+      // corresponding queue
+
+      waddr_queues[current_input_id].push(current_input);
+      if (kTransactionsVerbose) {
+        if (!iteration_announced) {
+          iteration_announced = true;
+          std::cout << std::endl << "Step " << std::dec << i << std::endl;
+        }
+        std::cout << std::dec << current_input_id << " inputs " << std::hex
+                  << current_input << std::endl;
+      }
+
+      // Renew the input data if the input handshake has been successful
+      current_input_id = identifiers[rand() % num_identifiers];
+      current_content = (uint64_t)(rand() & tb->simmem_get_content_mask());
+    }
+    if (requester_req_output_data) {
+      // If the output handshake has been successful, then add the output to the
+      // corresponding queue
+      if (tb->simmem_output_data_fetch(current_output)) {
+        wresp_queues[identifiers[(current_output &
+                                  tb->simmem_get_identifier_mask()) >>
+                                 kMsgWidth]]
+            .push(current_output);
+
+        if (kTransactionsVerbose) {
+          if (!iteration_announced) {
+            iteration_announced = true;
+            std::cout << std::endl << "Step " << std::dec << i << std::endl;
+          }
+          std::cout << std::dec
+                    << (uint64_t)(current_output &
+                                  ~tb->simmem_get_identifier_mask())
+                    << " outputs " << std::hex << current_output << std::endl;
+        }
+      }
+    }
+
+    tb->simmem_tick();
+
+    // Reset all signals after tick (they may be set again before the next DUT
+    // evaluation during the beginning of the next iteration)
+
+    if (reserve) {
+      tb->simmem_reservation_stop();
+    }
+    if (requester_apply_input_data) {
+      tb->simmem_input_data_stop();
+    }
+    if (requester_req_output_data) {
+      tb->simmem_output_data_stop();
+    }
+  }
+
+  tb->simmem_requests_complete();
+  while (!tb->simmem_is_done()) {
+    tb->simmem_tick();
+  }
+
+  // Check the input and output queues for mismatches
+  size_t nb_mismatches = 0;
+  for (size_t i = 0; i < num_identifiers; i++) {
+    while (!waddr_queues[i].empty() && !wresp_queues[i].empty()) {
+      current_input = waddr_queues[i].front();
+      current_output = wresp_queues[i].front();
+
+      waddr_queues[i].pop();
+      wresp_queues[i].pop();
+      if (kPairsVerbose) {
+        std::cout << std::hex << current_input << " - " << current_output
+                  << std::endl;
+      }
+      nb_mismatches += (size_t)(current_input != current_output);
+    }
+  }
+  if (kPairsVerbose) {
+    std::cout << std::endl
+              << "Mismatches: " << std::dec << nb_mismatches << std::endl
+              << std::endl;
+  }
+
+  return nb_mismatches;
 }
 
 int main(int argc, char **argv, char **env) {
