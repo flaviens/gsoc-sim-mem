de9b766c013325583257e708d2316e0fe7569481
Flavien Solt <flsolt@ethz.ch>
2020-08-28 11:02:00 +0200

    Sorted simmem top AXI interfaces

--- rtl/simmem_top.sv
+++ rtl/simmem_top.sv
@@ -8,42 +8,49 @@ module simmem_top (
     input logic clk_i,
     input logic rst_ni,
 
+    // AXI slave interface
+
     input  logic raddr_in_valid_i,
-    input  logic raddr_out_ready_i,
     output logic raddr_in_ready_o,
-    output logic raddr_out_valid_o,
+    input simmem_pkg::raddr_t raddr_i,
 
     input  logic waddr_in_valid_i,
-    input  logic waddr_out_ready_i,
     output logic waddr_in_ready_o,
-    output logic waddr_out_valid_o,
+    input simmem_pkg::waddr_t waddr_i,
 
     input  logic wdata_in_valid_i,
-    input  logic wdata_out_ready_i,
     output logic wdata_in_ready_o,
+    input simmem_pkg::wdata_t wdata_i,
+
+    input  logic rdata_out_ready_i,
+    output logic rdata_out_valid_o,
+    output simmem_pkg::rdata_t rdata_o,
+
+    input  logic wrsp_out_ready_i,
+    output logic wrsp_out_valid_o,
+    output simmem_pkg::wrsp_t  wrsp_o,
+
+    // AXI master interface
+
+    input  logic waddr_out_ready_i,
+    output logic waddr_out_valid_o,
+    output simmem_pkg::waddr_t waddr_o,
+
+    input  logic raddr_out_ready_i,
+    output logic raddr_out_valid_o,
+    output simmem_pkg::raddr_t raddr_o,
+
+    input  logic wdata_out_ready_i,
     output logic wdata_out_valid_o,
+    output simmem_pkg::wdata_t wdata_o,
 
     input  logic rdata_in_valid_i,
-    input  logic rdata_out_ready_i,
     output logic rdata_in_ready_o,
-    output logic rdata_out_valid_o,
+    input simmem_pkg::rdata_t rdata_i,
 
     input  logic wrsp_in_valid_i,
-    input  logic wrsp_out_ready_i,
     output logic wrsp_in_ready_o,
-    output logic wrsp_out_valid_o,
-
-    input simmem_pkg::raddr_t raddr_i,
-    input simmem_pkg::waddr_t waddr_i,
-    input simmem_pkg::wdata_t wdata_i,
-    input simmem_pkg::rdata_t rdata_i,
-    input simmem_pkg::wrsp_t  wrsp_i,
-
-    output simmem_pkg::raddr_t raddr_o,
-    output simmem_pkg::waddr_t waddr_o,
-    output simmem_pkg::wdata_t wdata_o,
-    output simmem_pkg::rdata_t rdata_o,
-    output simmem_pkg::wrsp_t  wrsp_o
+    input simmem_pkg::wrsp_t  wrsp_i
 );
 
   import simmem_pkg::*;
